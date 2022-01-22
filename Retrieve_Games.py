62e42f017faac73cb516ae4ad4efdd61026f1ee7
Charles-Bryan <charles.c.b2@outlook.com>
2022-01-22 15:25:10 -0500

    Automatically create the API url for Lichess

--- Retrieve_Games.py
+++ Retrieve_Games.py
@@ -6,31 +6,82 @@ Build Process
 2. Then compare if it is faster tot clean the data in the datatable vs when parsing the games
       a) Likely leaving the actual game text until already in a data table regardless
 """
-import requests # for downloading games
+import requests  # for downloading games
 import cProfile
 import pandas as pd
+import datetime as dt
+
 
 def create_url(inputs):
-    # default
+    # default parts
     prefix = f"https://lichess.org/api/games/user/{inputs['Player']}?"
-    mid = f"tags=false&clocks=false&evals=false&opening=false"
+    # tags=true is required for any details other than just moves
+    mid = f"tags=true&clocks=false&evals=false&opening=false"
 
     # pre-mid sections. These will append a '&' as a suffix
+
     # 1. color
     match inputs['Player_Color']:
         case 'Both':
-            print('Both')
+            color_str = ''
         case 'White':
-            print('White')
+            color_str = 'color=white&'
         case 'Black':
-            print('Black')
+            color_str = 'color=black&'
+
+    # 2. opponent
+    if inputs['Opponent'] == 'All':
+        opponent_str = ''
+    else:
+        opponent_str = f"vs={inputs['Opponent']}&"
+
+    # 3. mode (rated or casual)
+    match inputs['Mode']:
+        case 'Both':
+            mode_str = ''
+        case 'Rated':
+            mode_str = 'rated=true&'
+        case 'Casual':
+            mode_str = 'rated=false&'
+
     # post-mid sections. These sections will prefix the '&' and not append
-    # start/end dates
+
+    # 4. start date
+    if inputs['Start_Date'] == 'None':
+        start_date_str = ''
+    else:
+        # start_dt = fun(inputs['Start_Date'])
+        t_ms = int((inputs['Start_Date'] - dt.datetime(1970, 1, 1)).total_seconds()) * 1000
+        start_date_str = f'&since={t_ms}'
+
+    # 5. end date
+    if inputs['End_Date'] == 'None':
+        end_date_str = ''
+    else:
+        t_ms = int((inputs['End_Date'] - dt.datetime(1970, 1, 1)).total_seconds()) * 1000
+        end_date_str = f'&until={t_ms}'
+
+    # 6. gametypes
+    available_gametypes = {
+        "UltraBullet": 'ultraBullet',
+        "Bullet": 'bullet',
+        "Blitz": 'blitz',
+        "Rapid": 'rapid',
+        "Classical": 'classical',
+        "Correspondence": 'correspondence'
+    }
+    selected_gametypes = []
+    for key, value in available_gametypes.items():
+        if inputs[key]:
+            selected_gametypes.append(value)
+    gametypes_str = f"&perfType={'%2C'.join(selected_gametypes)}"
+
+    return prefix + color_str + opponent_str + mode_str + mid + start_date_str + end_date_str + gametypes_str
 
 
-def retrieve_data(site="Lichess", user="E4_is_Better"):
+def retrieve_data(url):
     """
-    Starting off with no filters
+    Just finished making the url
     """
     default_game = {
         'Event': None,
@@ -51,8 +102,7 @@ def retrieve_data(site="Lichess", user="E4_is_Better"):
         'Termination': None,
         'Raw_Moves': None
     }
-    # https://lichess.org/api/games/user/E4_is_Better?tags=true&clocks=false&evals=false&opening=false
-    url = 'https://lichess.org/api/games/user/' + user + '?tags=true&clocks=false&evals=false&opening=false'
+
     r = requests.get(url, stream=True)
 
     all_games = []
@@ -65,12 +115,14 @@ def retrieve_data(site="Lichess", user="E4_is_Better"):
             if line[0] == '[':
                 space_loc = line.find(' ')
                 # temp_game[line[1:space_loc]] = line[space_loc+2:-2]  # Use this after testing
-                if line[1:space_loc] not in default_game.keys():  # Easier/Safer to just compare vs a white list then to make this black list
+                if line[
+                   1:space_loc] not in default_game.keys():  # Easier/Safer to just compare vs a white list then to make this black list
                     ignore_game = True
                 elif temp_game[line[1:space_loc]] == None:
-                     temp_game[line[1:space_loc]] = line[space_loc + 2:-2]
+                    temp_game[line[1:space_loc]] = line[space_loc + 2:-2]
                 else:
-                    print(f"Error! Updating {line[1:space_loc]} from {temp_game[line[1:space_loc]]} to {line[space_loc+2:-2]}. It should be None if we are going to update it.")
+                    print(
+                        f"Error! Updating {line[1:space_loc]} from {temp_game[line[1:space_loc]]} to {line[space_loc + 2:-2]}. It should be None if we are going to update it.")
             elif line[0].isdigit():
                 temp_game['Raw_Moves'] = line
                 if ignore_game:  # Need to catch variants here like Crazyhouse
@@ -102,6 +154,7 @@ def retrieve_data(site="Lichess", user="E4_is_Better"):
     # Not sure the best way to transpose in datatable, so doing it in numpy first....
     return pd.DataFrame(all_games)
 
+
 def process_df_cols(input_df):
     df = input_df.copy()
     # Event - Likely Drop and use the Rating Dif to determine if rated or casual
@@ -119,37 +172,47 @@ def process_df_cols(input_df):
     # Variant - Likely Drop. Games will be selected through API
     # TimeControl - Likely Drop
     # ECO - Likely Drop
-    # Termination - Will need to see what unique options there are and if that will affect dropping rows. Use a player like german11 to see all possibilities.
+    # Termination - Will need to see what unique options there are and if that will affect dropping rows.
+    #   Use a player like german11 to see all possibilities.
 
     # TimeControl
     #   Splitting into StartClock(in seconds) and increment(in seconds)
     df['TimeControl'] = df['TimeControl'].str.replace('-', '99999+0')
     df[['StartClock', 'Increment']] = df['TimeControl'].str.split('+', n=1, expand=True).apply(pd.to_numeric)
 
-    #testing
+    # testing
+
     print(0)
-    test = df["StartClock"] + 40*df["Increment"]
+    test = df["StartClock"] + 40 * df["Increment"]
     print(0)
+
+    return df
+
 
 def main():
     inputs = {
         "Player": 'E4_is_Better',
-        "Player_Color": 'Both',     # White, Black, Both
-        "Opponent": 'All',          # Username, All
-        "UltraBullet": 'Yes',       # Yes, No
-        "Bullet": 'Yes',            # Yes, No
-        "Blitz": 'Yes',             # Yes, No
-        "Rapid": 'Yes',             # Yes, No
-        "Classical": 'Yes',         # Yes, No
-        "Correspondence": 'Yes',    # Yes, No
-        "Mode": 'Both',             # Rated, Casual, Both
-        "Start_Date": '1990-01-30', # Dates in a format undecided
-        "End_Date": 'Today'         # Today or date in format above
+        "Player_Color": 'Both',  # White, Black, Both
+        "Opponent": 'All',  # Username, All
+        "UltraBullet": True,  # True, False
+        "Bullet": False,  # True, False
+        "Blitz": True,  # True, False
+        "Rapid": True,  # True, False
+        "Classical": False,  # True, False
+        "Correspondence": False,  # True, False
+        "Mode": 'Both',  # Rated, Casual, Both
+        "Start_Date": dt.datetime(2010, 1, 22, 0, 0),  # None, datetime object from plotly
+        "End_Date": 'None',  # None, datetime object from plotly
+        "Site": 'Lichess'  # Lichess. Maybe expand to Chess.com one day
     }
     url = create_url(inputs)
-    df = retrieve_data()
-    df = process_df_cols(df)
+    raw_game_df = retrieve_data(url)
+
+    cleaned_game_df = process_df_cols(raw_game_df)
 
 
 if __name__ == '__main__':
     cProfile.run('main()')
+
+
+
